05a76f18eb5a8636135959cd72e1ac32b98c828c
Andrew Walsh <awalsh128@gmail.com>
2016-03-30 22:35:34 -0700

    Filled in assembly information and added versioning.

--- PcapngFile/Properties/AssemblyInfo.cs
+++ PcapngFile/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PcapngFile")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("PCAP-NG File Reader")]
+[assembly: AssemblyDescription("Library for reading PCAP-NG format files.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Awalsh128")]
 [assembly: AssemblyProduct("PcapngFile")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 
 [assembly: InternalsVisibleTo("PcapngFile.Tests")]
