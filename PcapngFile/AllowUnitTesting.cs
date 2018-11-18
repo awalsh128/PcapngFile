084ee81719a1a9b332eab69a7d538197e9cccc9b
nloum <39776302+nloum@users.noreply.github.com>
2018-11-18 11:23:10 -0600

    Port to .NET Standard

---
+++ PcapngFile/AllowUnitTesting.cs
@@ -0,0 +1,4 @@
+﻿// Allow the unit tests project to see types internal to this assembly
+// See https://stackoverflow.com/a/42235577
+using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("PcapngFile.Tests")]
